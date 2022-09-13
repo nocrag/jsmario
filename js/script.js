a3ad0d225659ef091e995bb20d9017baae1656f9
nocrag <joaoggr@hotmail.com>
2022-09-13 15:54:40 -0300

    added sound and start button

--- js/script.js
+++ js/script.js
@@ -1,8 +1,24 @@
 const mario = document.querySelector('.mario');
 const pipe = document.querySelector('.pipe');
+const gameOver = document.querySelector('.game-over');
+const replay = document.querySelector('.replay');
+const jumpAudio = document.getElementById('jump-audio');
+const gameOverAudio = document.getElementById('game-over-audio');
+const marioSong = document.getElementById('mario-song');
+const startButton = document.querySelector('.start');
+
+pipe.style.display = 'none';
+
+const start = () => {
+    pipe.classList.add('animation-pipe')
+    startButton.style.display = 'none';
+    pipe.style.display = 'initial';
+}
+
 
 const jump = () => {
     mario.classList.add('jump')
+    jumpAudio.play();
 
     setTimeout(() => {
         mario.classList.remove('jump');
@@ -20,13 +36,19 @@ const loop = setInterval(() =>{
 
         mario.style.animation = 'none';
         mario.style.bottom = `${marioPosition}px`
-
-        mario.src = './images/game-over.png';
         mario.style.width = '75px';
         mario.style.margin = '50px';
+        mario.src = './images/game-over.png';
+        
+        gameOver.src= './images/game-over-font.png';
+        replay.src= './images/replay.png';
+        
+        gameOverAudio.play()
+        marioSong.pause()
 
         clearInterval(loop);
     }
 }, 10)
 
 document.addEventListener('keydown', jump);
+startButton.addEventListener('click', start);
